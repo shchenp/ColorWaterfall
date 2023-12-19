6417f5b31190610962f6fdc6e6a472250e99144d
Nikita <np.shche@gmail.com>
2023-12-19 23:21:34 +0300

    remove gizmo

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -28,32 +28,10 @@ public class CubeSpawner : MonoBehaviour
          {
             var item = Instantiate(_itemPrefab, transform);
             item.transform.position = _grid.CellToWorld(new Vector3Int(x, 0, z));
-            Debug.Log(item.transform.position);
+            
             yield return new WaitForSeconds(_spawnTime);
          }
       }
       
-   }
-
-   private void OnDrawGizmos()
-   {
-      var cellSize = _grid.cellSize;
-      var cellGap = _grid.cellGap;
-      var origin = _grid.transform.position;
-      
-      Gizmos.color = Color.yellow;
-      
-      for (int x = 0; x < _itemCountHorizontal; x++)
-      {
-         for (int z = 0; z < _itemCountVertical; z++)
-         {
-            var position = origin + new Vector3(x * (cellSize.x + cellGap.x), 0, z * (cellSize.z + cellGap.z));
-            
-            Gizmos.DrawLine(position, position + new Vector3(cellSize.x, 0, 0));
-            Gizmos.DrawLine(position, position + new Vector3(0, 0, cellSize.z));
-            Gizmos.DrawLine(position + new Vector3(cellSize.x, 0, 0), position + new Vector3(cellSize.x, 0, cellSize.z));
-            Gizmos.DrawLine(position + new Vector3(0, 0, cellSize.z), position + new Vector3(cellSize.x, 0, cellSize.z));
-         }
-      }
    }
 }
